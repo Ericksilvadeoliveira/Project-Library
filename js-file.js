cc32152225e8fad0209c74463af5db2ed0cae1f4
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-11-21 15:41:05 -0300

    css ok. adicionar values no javascript

--- js-file.js
+++ js-file.js
@@ -1,10 +1,35 @@
-let myLibrary = [];
+const formArea = document.querySelector("#form-content");
+const addBook = document.querySelector(".add-book");
 
-function book () {
-    const author = document.querySelector("#author.value");
-    this.author = author;
+addBook.addEventListener("click", ()=> {
+    if (formArea.style.display === 'none') {
+        formArea.style.display = 'block';
+        document.getElementById('form-book').style.width = '400px';
+    } else {
+        formArea.style.display = 'none';
     }
+})
 
-function addBookToLibrary() {
-
-}
+// class myLibrary {
+//    constructor() {
+//       this.books = []
+//    }
+//}
+//
+//class book {
+//    constructor(
+//        title = '',
+//        author = '',
+//        pages = '',
+//        read = false
+//    ) {
+//       this.title = title
+//       this.author = author
+//       this.pages = pages
+//       this.read = read 
+//    }
+//}
+//
+//function addBookToLibrary() {
+//
+//}
